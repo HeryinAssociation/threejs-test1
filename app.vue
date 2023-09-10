8c6d386c029e5b5f639715d3ede6d300807a79e5
xiaoyu01 <xiaoyu01@njfu.edu.cn>
2023-09-10 20:30:15 +0800

    init commit

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <TresCanvas window-size shadows alpha>
+      <TresPerspectiveCamera :position="[20, 50, 50]" :fov="70" />
+      <OrbitControls :target="[0, 15, 0]" />
+      <TresMesh>
+        <Suspense>
+          <GLTFModel path="/models/3.glb" draco ref="modelRef" />
+        </Suspense>
+      </TresMesh>
+      <!-- <TresDirectionalLight :position="[-4, 8, 4]" :intensity="1.5" cast-shadow /> -->
+      <TresAmbientLight :intensity="5" />
+    </TresCanvas>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { OrbitControls, GLTFModel } from "@tresjs/cientos";
+
+// const { scene } = await useGLTF("/models/test.glb");
+</script>
---
+++ components/Canva.vue
@@ -0,0 +1,21 @@
+<template>
+  <TresPerspectiveCamera :position="[3, 3, 3]" :fov="45" :look-at="[0, 0, 0]" />
+  <!-- <OrbitControls /> -->
+  <TresOrbitControls v-if="renderer" :args="[camera, renderer?.domElement]" />
+  <TresMesh>
+    <Suspense>
+      <GLTFModel path="/models/3.glb" ref="modelRef" />
+    </Suspense>
+  </TresMesh>
+  <!-- <TresDirectionalLight :position="[-4, 8, 4]" :intensity="1.5" cast-shadow /> -->
+  <TresAmbientLight :intensity="5" />
+</template>
+<script lang="ts" setup>
+import { extend, useTresContext } from "@tresjs/core";
+import { GLTFModel } from "@tresjs/cientos";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+extend({ OrbitControls });
+// const { scene } = await useGLTF("/models/test.glb");
+const { renderer, camera } = useTresContext();
+</script>
